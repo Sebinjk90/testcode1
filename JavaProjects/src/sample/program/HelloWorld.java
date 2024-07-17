95eac13729fa1e44c734eab455696efe4ff6924f
sebin <sebin@LAPTOP-CL23BE85.speedport.ip>
2024-07-17 11:24:38 +0530

    hello world

---
+++ JavaProjects/src/sample/program/HelloWorld.java
@@ -0,0 +1,14 @@
+package sample.program;
+
+public class HelloWorld {
+public void method()
+{
+	System.out.println("Hello World");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HelloWorld obj=new HelloWorld();
+		obj.method();
+	}
+
+}
